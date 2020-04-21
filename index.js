8688e5f2c22939146866a9d63a7642a90b0596b0
Robson João <robson.luz@bomjesus.br>
2020-04-21 23:50:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var hbs = require('express-hbs');
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.get('/sobre', function (req, res) {
+  res.render('sobre');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
