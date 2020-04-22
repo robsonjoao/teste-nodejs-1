a986ec8d96f46404f9d19e894cf675d4f0ebfe7f
Robson João Padilha da Luz <robson.luz@bomjesus.br>
2020-04-22 00:19:23 +0000

    alterado porta

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get('/sobre', function (req, res) {
   res.render('sobre');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
